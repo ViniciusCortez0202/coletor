c8ec928778f96fc384b8770dede1a64f1b359028
Ramon Barros <ramonbarros6@gmail.com>
2024-03-21 14:40:19 -0300

    wip position page

--- lib/position_page.dart
+++ lib/position_page.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'dart:async';
+import 'package:flutter/scheduler.dart';
 
 class PositionPage extends StatefulWidget {
   const PositionPage({Key? key}) : super(key: key);
@@ -12,8 +15,49 @@ class PositionPage extends StatefulWidget {
 class _PositionPageState extends State<PositionPage> {
   int currentX = 0;
   int currentY = 0;
+  final List<String> allowedUUIDs = ["41:99:D8:90:55:2A", "72:14:41:C2:46:03"];
+  List<BluetoothDevice> _systemDevices = [];
+  
+  List<ScanResult> _scanResults = [];
+  BluetoothAdapterState _adapterState = BluetoothAdapterState.unknown;
+  late StreamSubscription<List<ScanResult>> _scanResultsSubscription;
+  late StreamSubscription<bool> _isScanningSubscription;
+  late StreamSubscription<BluetoothAdapterState> _adapterStateStateSubscription;
+  bool _isScanning = false;
+  Duration timeToScan = const Duration(seconds: 10);
+  late double duration;
+  late Timer _timer;
 
-  Future<void> fetchData(List<int> rssiValues) async {
+  @override
+  void initState() {
+    duration = timeToScan.inSeconds.toDouble();
+    super.initState();
+    
+    Timer.periodic(Duration(seconds: 5), (timer) {
+      if (!_isScanning) {
+        startScan();
+      }
+    });
+
+    _adapterStateStateSubscription =
+        FlutterBluePlus.adapterState.listen((state) {
+      _adapterState = state;
+    });
+
+    _scanResultsSubscription = FlutterBluePlus.scanResults.listen((results) {
+      results.forEach((element) {
+        _scanResults.add(element);
+        if (allowedUUIDs.contains(element.device.id.toString())) {
+        }
+      });
+    }, onError: (e) {});
+
+    _isScanningSubscription = FlutterBluePlus.isScanning.listen((state) {
+      _isScanning = state;
+    });
+  }
+
+  Future<void> fetchData(List<int?> rssiValues) async {
     final response = await http.get(
       Uri.parse('https://rei-dos-livros-api-f270d083e2b1.herokuapp.com/knn_position?rssis=${rssiValues.join(",")}'),
       headers: {
@@ -32,16 +76,65 @@ class _PositionPageState extends State<PositionPage> {
     }
   }
 
-  @override
-  void initState() {
-    super.initState();
-    fetchData([12, -12, -14, 12]);
+  Future<void> startScan() async {
+    try {
+      await FlutterBluePlus.startScan(timeout: timeToScan);
+      duration = timeToScan.inSeconds.toDouble();
+      print('Scanning for devices...');
+    } catch (e) {
+      print("Erro ao iniciar o scan: $e");
+    }
+
+    _scanResults.clear();
+
+    _scanResultsSubscription = FlutterBluePlus.scanResults.listen((results) {
+      Map<String, List<int>> groupedResults = {};
+
+      results.forEach((element) {
+        String uuid = element.device.id.toString();
+        int rssiValue = element.rssi;
+
+        if (groupedResults.containsKey(uuid)) {
+          groupedResults[uuid]!.add(rssiValue);
+        } else {
+          groupedResults[uuid] = [rssiValue];
+        }
+      });
+
+      List<int?> rssisArrays = [];
+      
+      groupedResults.values.forEach((rssis) {
+        if (rssis.isNotEmpty) {
+          rssisArrays.add(rssis[0]);
+        } else {
+          rssisArrays.add(null);
+        }
+      });
+
+      int maxLength = 7;
+
+      rssisArrays.length < maxLength
+          ? rssisArrays.addAll(List<int?>.filled(maxLength - rssisArrays.length, null))
+          : null;
+
+      rssisArrays = rssisArrays.sublist(0, maxLength);
+
+      print("O ARRAY DE RSSIS:");
+      print(rssisArrays);
+
+      fetchData(rssisArrays);
+
+
+    }, onError: (e) {
+      print("Erro ao escanear: $e");
+    });
   }
+
 
   @override
   Widget build(BuildContext context) {
     final int rows = 3;
-    final int cols = 4;
+    final int cols = 3;
 
     return Scaffold(
       appBar: AppBar(
