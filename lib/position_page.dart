c26af3a0c3a2e4b7bb12cbccf476ec205cde173b
Ramon Barros <ramonbarros6@gmail.com>
2024-04-18 23:38:58 -0300

    wip

--- lib/position_page.dart
+++ lib/position_page.dart
@@ -12,9 +12,9 @@ import 'package:geolocator/geolocator.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:open_settings/open_settings.dart';
 import 'package:coletor/permission_services.dart';
-
 import 'package:coletor/controller.dart';
 import 'package:coletor/permission_services.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class PositionPage extends StatefulWidget {
   const PositionPage({Key? key}) : super(key: key);
@@ -34,47 +34,12 @@ class _PositionPageState extends State<PositionPage> {
   String beaconUUID = 'F7826DA6-4FA2-4E98-8024-BC5B71E0893E';
   StreamSubscription<RangingResult>? _streamRanging;
   StreamSubscription<BluetoothState>? _streamBluetooth;
-  //String beaconUUID = '00:FA:B6:1D:DE:07';
 
   @override
   void initState() {
     super.initState();
-    // _checkLocationPermission();
-    // _checkBluetoothPermission();
+
     listeningState();
-  }
-
-    _checkLocationPermission() async {
-    try {
-      bool status = await PermissionService.checkLocationPermission();
-      if (status) {
-        print(Permission.bluetoothScan.status);
-      } else {
-        print(Permission.bluetoothScan.status);
-        bool locationPermissionStatus = await PermissionService.requestLocationPermission(context);
-        if (locationPermissionStatus) {
-          showBluetoothDialog();
-        }
-      }
-    } catch (e) {
-      print(e);
-    }
-  }
-
-    _checkBluetoothPermission() async {
-    try {
-      bool status = await PermissionService.checkBluetoothPermission();
-      if (status) {
-        print('Bluetooth Permission: ${Permission.bluetoothScan.status}');
-      } else {
-        print('Bluetooth Permission: ${Permission.bluetoothScan.status}');
-        bool bluetoothPermissionStatus =
-            await PermissionService.requestBluetoothPermission(context);
-        if (bluetoothPermissionStatus) {
-          showBluetoothDialog();
-        }
-      }
-    } catch (e) {}
   }
 
   void listeningState() async {
@@ -95,7 +60,7 @@ class _PositionPageState extends State<PositionPage> {
     final regions = <Region>[];
 
     regions.add(
-        Region(identifier: 'com.beacon', proximityUUID: "F7826EE6-4FA2-4E98-8024-BC5B71E0893E"));
+        Region(identifier: 'com.beacon', proximityUUID: "F7826DA6-4FA2-4E98-8024-BC5B71E0893E"));
 
     _streamRanging = flutterBeacon.ranging(regions).listen((RangingResult result) {
       if (result != null && result.beacons.isNotEmpty) {
@@ -106,55 +71,6 @@ class _PositionPageState extends State<PositionPage> {
       debugPrint('No beacons found');
     }
     });
-  }
-
-  showBluetoothDialog() {
-    print('bluetooth dialog');
-    if (Platform.isIOS) {
-      return CupertinoAlertDialog(
-        title: const Text("UJ Wayfinder Location"),
-        content: const Text(
-            'App wants your bluetooth to be turn ON to enable scanning of beacon around you to enhance your navigation experience.'),
-        actions: <Widget>[
-          CupertinoDialogAction(
-            isDefaultAction: true,
-            child: const Text("Yes"),
-            onPressed: () async {
-              Navigator.pop(context);
-              await Permission.bluetoothScan.request();
-              Navigator.pop(context);
-            },
-          ),
-          CupertinoDialogAction(
-            child: const Text("No"),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          )
-        ],
-      );
-    } else {
-      return showDialog(
-          context: context,
-          builder: (context) {
-            return AlertDialog(
-              content: const Text(
-                  'App wants your bluetooth to be turn ON to enable scanning of beacon around you to enhance your navigation experience.'),
-              actions: [
-                TextButton(
-                    onPressed: () async {
-                      OpenSettings.openBluetoothSetting();
-                    },
-                    child: const Text('Allow')),
-                TextButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: const Text('Deny'))
-              ],
-            );
-          });
-    }
   }
 
   // Future<void> fetchData(List<int?> rssiValues) async {
